71110621a958d4edd3ccfdcd3af7d9047a2f6877
Vimal Buckley <vbuckley68@claytonschools.net>
2024-07-24 19:21:45 -0500

    Changed shooterIO aim from translation to angle input

--- src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
@@ -1,9 +1,12 @@
 package frc.robot.subsystems.shooter;
 
-import java.util.function.Supplier;
+import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utilities.EZLogger.LogAccess;
@@ -22,6 +25,16 @@ public abstract class ShooterIO extends SubsystemBase implements Loggable {
         return instance;
     }
 
+    protected final InterpolatingDoubleTreeMap ANGLE_MAP = new InterpolatingDoubleTreeMap();
+
+    public double getBestAngle(Translation2d currentPose) {
+        Translation2d speakerPose = new Translation2d(
+            DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Blue ?
+            -0.5 : 16.5, 5.9
+        );
+        return ANGLE_MAP.get(speakerPose.getDistance(currentPose));
+    }
+
     @Override
     public void log(LogAccess table) {
         ShooterState state = getState();
@@ -33,7 +46,7 @@ public abstract class ShooterIO extends SubsystemBase implements Loggable {
         table.put("Target Shooter Speed", state.targetShooterSpeed);
     }
 
-    public abstract Command aim(Supplier<Translation2d> translation);
+    public abstract Command aim(DoubleSupplier angle);
     public abstract Command load(double speed);
     public abstract Command shooter(double speed);
     public abstract ShooterState getState();
