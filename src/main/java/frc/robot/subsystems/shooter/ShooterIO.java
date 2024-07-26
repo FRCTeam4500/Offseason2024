4793b2b597328bfc06e6ad03bb56adf34e5f35ce
Vimal Buckley <vbuckley68@claytonschools.net>
2024-07-25 21:10:15 -0500

    Hopefully finished ShooterSim

--- src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
@@ -1,12 +1,13 @@
 package frc.robot.subsystems.shooter;
 
 import java.util.function.DoubleSupplier;
+import java.util.function.Supplier;
 
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
-import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.wpilibj.RobotBase;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utilities.EZLogger.LogAccess;
@@ -19,20 +20,24 @@ public abstract class ShooterIO extends SubsystemBase implements Loggable {
             if (RobotBase.isReal()) {
                 instance = null;
             } else {
-                instance = null;
+                instance = new ShooterSim();
             }
         }
         return instance;
     }
 
-    protected final InterpolatingDoubleTreeMap ANGLE_MAP = new InterpolatingDoubleTreeMap();
-
-    public double getBestAngle(Translation2d currentPose) {
-        Translation2d speakerPose = new Translation2d(
-            DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Blue ?
-            -0.5 : 16.5, 5.9
-        );
-        return ANGLE_MAP.get(speakerPose.getDistance(currentPose));
+    protected final Constraints SHOOTER_CONSTRAINTS = new Constraints(180, 360);
+    protected static final InterpolatingDoubleTreeMap ANGLE_MAP = new InterpolatingDoubleTreeMap();
+    public static final double FF_OFFSET = 26.5;
+    public final MechanismLigament2d SHOOTER_MECH = new MechanismLigament2d("Shooter", 0.3, -38);
+    static {
+        ANGLE_MAP.put(1.37, -44.428);
+        ANGLE_MAP.put(1.74, -36.71);
+        ANGLE_MAP.put(2.14, -26.43);
+        ANGLE_MAP.put(2.8, -21.29);
+        ANGLE_MAP.put(3.25, -18.71);
+        ANGLE_MAP.put(3.75, -16.14);
+        ANGLE_MAP.put(4.4, -14.34);
     }
 
     @Override
@@ -44,9 +49,11 @@ public abstract class ShooterIO extends SubsystemBase implements Loggable {
         table.put("Target Loader Speed", state.targetLoaderSpeed);
         table.put("Current Shooter Speed", state.currentShooterSpeed);
         table.put("Target Shooter Speed", state.targetShooterSpeed);
+        SHOOTER_MECH.setAngle(state.currentTilt );
     }
 
     public abstract Command aim(DoubleSupplier angle);
+    public abstract Command aim(Supplier<Translation2d> pose);
     public abstract Command load(double speed);
     public abstract Command shooter(double speed);
     public abstract ShooterState getState();
